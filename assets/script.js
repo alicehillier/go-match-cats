8af3314fea88504c151457529d5032fcd2f33c5a
Alice <alice_hillier@hotmail.com>
2023-06-20 22:38:56 +0100

    add transitions and flip class to cards. add startGame function to js file which counts down before letting the player interact with cards

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,64 @@
+// retrieves the start button and adds event listener to trigger the startGame function below.
+let startButton = document.getElementById('start-button');
+startButton.addEventListener('click', startGame);
+
+let restartButton = document.getElementsByClassName('restart');
+restartButton[0].addEventListener('click', restartGame);
+
+/**Removes the Start button when it's clicked on and counts down "3, 2, 1, GO!" with one second between each of them */
+function startGame() {
+    // remove the start button after one second and display "3" in its place.
+    startButton.remove();
+    let three = document.createElement('p');
+    three.classList.add('start-countdown');
+    three.innerHTML = "3";
+    let cardsGrid = document.getElementsByClassName('cards-grid');
+    cardsGrid[0].append(three);
+    setTimeout(() => {
+        // remove the "3" after one second and display "2" in its place.
+        three.remove();
+        console.log('three removed');
+        let two = document.createElement('p');
+        two.classList.add('start-countdown');
+        two.innerHTML = "2";
+        cardsGrid[0].append(two);
+        setTimeout(() => {
+            // remove the "2" after one second and display "1" in its place.
+            two.remove();
+            console.log('two removed');
+            let one = document.createElement('p');
+            one.classList.add('start-countdown');
+            one.innerHTML = "1";
+            cardsGrid[0].append(one);
+            setTimeout(() => {
+                // remove the "1" after one second and display "GO!" in its place.
+                one.remove();
+                console.log('one removed');
+                let go = document.createElement('p');
+                go.classList.add('start-countdown');
+                go.innerHTML = "GO!";
+                cardsGrid[0].append(go);
+                setTimeout(() => {
+                    go.remove();
+                    console.log('go removed');
+                    // immediately after "GO!" is removed, start the timer by triggering the startTimer function.
+                    startTimer();
+                    // the flipCard function is also triggered here so players cannot flip cards before clicking on the start button and waiting for the countdown to end.
+                    flipCard();
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}
+
+function startTimer() {
+    console.log('start timer now!');
+}
+
+function restartGame() {
+    console.log('restart game now');
+}
+
 let flippedCard = false;
 let firstCard;
 let secondCard;
@@ -35,3 +96,4 @@ function flipCard() {
         }
     }
 };
+
