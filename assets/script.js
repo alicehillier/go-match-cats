bc6d3dd4a2b73be625259f798bc42778042fc720
Alice <alice_hillier@hotmail.com>
2023-07-03 19:29:19 +0100

    re-order functions and add boardLocked=true to relevant functions so players cannot flip cards after a game has ended, or at the beginning of a new game,  no matter whether they won, lost or clicked on the restart button

--- assets/script.js
+++ assets/script.js
@@ -53,156 +53,6 @@ function showInstructions() {
     }
 }
 
-/**Removes the Start button when it's clicked on and counts down "3, 2, 1, GO!" with one second between each of them. */
-function startGame() {
-    // remove the start button after one second and display "3" in its place.
-    startButton.remove();
-    let countdown = document.createElement('p');
-    countdown.classList.add('start-countdown');
-    countdown.innerHTML = "3";
-    let cardsGrid = document.getElementsByClassName('cards-grid');
-    cardsGrid[0].append(countdown);
-    setTimeout(() => {
-        // remove the "3" after one second and display "2" in its place.
-        countdown.innerHTML = "2";
-        setTimeout(() => {
-            // remove the "2" after one second and display "1" in its place.
-            countdown.innerHTML = "1";
-            setTimeout(() => {
-                // remove the "1" after one second and display "GO!" in its place.
-                countdown.innerHTML = "GO!";
-                setTimeout(() => {
-                    // remove the countdown.
-                    countdown.remove();
-                }, 1000);
-                // immediately after "GO!" is removed, start the timer by triggering the startTimer 
-                // function and let the user play by triggering the flipCard function.
-                startTimer();
-                flipCard();
-            }, 1000);
-        }, 1000);
-    }, 1000);
-}
-
-/**Starts the countdown from 60 seconds. If 0 is reached, the gameOver function is triggered. */
-function startTimer() {
-    let restartButton = document.createElement('button');
-    restartButton.setAttribute('class', 'restart');
-    restartButton.innerHTML = "RESTART";
-    let headerButtons = document.getElementsByClassName('header-buttons');
-    headerButtons[0].append(restartButton);
-    restartButton.addEventListener('click', restartMidGame);
-
-    console.log('start timer now!');
-    let gameTimer = document.getElementsByClassName('timer');
-    let maxTime = 30;
-    let timer = setInterval(() => {
-        /* If the timer has more than 0 seconds remaining, show how many seconds are left
-         in the gameTimer element. Then, deduct 1 second from the timer.*/
-        if (maxTime > 0) {
-            // If there is less than 10 seconds on the timer, add a '0' before the number of seconds.
-            gameTimer[0].innerHTML = `00:${maxTime < 10 ? '0' + maxTime : maxTime}`;
-            maxTime--;
-            // If all 24 cards have the card-flipped class, trigger the youWin function.
-            let cardFlipped = document.getElementsByClassName('card-flipped');
-            if (maxTime > 0 && cardFlipped.length === 24) {
-                clearInterval(timer);
-                gameTimer[0].innerHTML = "00:00";
-                youWin();
-                restartButton.remove();
-            }
-            if (maxTime > 0 && startAgain === true) {
-                clearInterval(timer);
-                gameTimer[0].innerHTML = "00:00";
-                startAgain = false;
-            }
-            /* If the timer reaches 0, show "00:00!" in the timer area, stop the timer
-            and trigger the gameOver function.*/
-        } else {
-            gameTimer[0].innerHTML = "00:00";
-            clearInterval(timer);
-            gameOver();
-            restartButton.remove();
-        }
-    }, 1000);
-}
-
-/**Stops the player from flipping cards and tells them the game is over. Gives the player the option to play again with a retry button. */
-function gameOver() {
-    console.log('gameOver triggered');
-    // get all elements with the class 'card' and iterate through them, adding the event listener for all.
-    let cards = document.getElementsByClassName('card');
-    for (let i = 0; i < cards.length; i++) {
-        cards[i].removeEventListener('click', flipCard);
-    }
-    // show "YOU LOST!" to the player.
-    let losingMessage = document.createElement('p');
-    losingMessage.classList.add('losing-message');
-    losingMessage.innerHTML = "YOU LOST!";
-    let cardsGrid = document.getElementsByClassName('cards-grid');
-    cardsGrid[0].append(losingMessage);
-    setTimeout(() => {
-        // remove the losingMessage after one second and display retryButton in its place.
-        losingMessage.remove();
-        console.log('losingMessage removed');
-        let retryButton = document.createElement('button');
-        retryButton.setAttribute('id', 'retry-button');
-        retryButton.innerHTML = "TRY AGAIN?";
-        cardsGrid[0].append(retryButton);
-        retryButton.addEventListener('click', restartGame);
-    }, 2000);
-}
-
-/**Removes the 'try again' button once clicked, reset the game area and re-add the start button with its event listener so the game can be run again. */
-function restartGame() {
-    console.log('restart game now');
-    // Remove the retry button as it has been clicked.
-    let retryButton = document.getElementById('retry-button');
-    retryButton.remove();
-    let cards = document.getElementsByClassName('card');
-    for (let i = 0; i < cards.length; i++) {
-        // cards[i].removeEventListener('click', flipCard);
-        // get all the cards and remove the 'flip' class, so they are all facing the same way.
-        cards[i].classList.remove('flip');
-        setTimeout(() => {
-            // make all cards visible again after 0.5 sec delay, so player cannot see the image on the cards as they turn.  
-            cards[i].style.visibility = "visible";
-        }, 500);
-    }
-    // re-add the startbutton and its event listener, so the startGame function can be run again.
-    // set timeout so button appears after cards have been returned to original positions.
-    setTimeout(() => {
-        cardsGrid[0].prepend(startButton);
-        startGame();
-    }, 500);
-}
-
-let startAgain = false;
-
-/**Restarts the game and the timer when the player is in the middle of the current game. */
-function restartMidGame() {
-    console.log('restart mid-game now');
-    // Remove the retry button as it has been clicked.
-    let restartButton = document.getElementsByClassName('restart');
-    restartButton[0].remove();
-    startAgain = true;
-    let cards = document.getElementsByClassName('card');
-    for (let i = 0; i < cards.length; i++) {
-        // cards[i].removeEventListener('click', flipCard);
-        // get all the cards and remove the 'flip' class, so they are all facing the same way.
-        cards[i].classList.remove('flip');
-        setTimeout(() => {
-            // make all cards visible again after 0.5 sec delay, so player cannot see the image on the cards as they turn.  
-            cards[i].style.visibility = "visible";
-        }, 500);
-    }
-    // re-add the startbutton and its event listener, so the startGame function can be run again.
-    // set timeout so button appears after cards have been returned to original positions.
-    setTimeout(() => {
-        cardsGrid[0].prepend(startButton);
-    }, 500);
-}
-
 //THE CODE IN THE flipCard FUNCTION WAS SOURCED FROM A FREECODECAMP TUTORIAL AND CUSTOMISED: https://www.youtube.com/watch?v=ZniVgo8U7ek
 //---------------------------------------------------------------------------------------------------------
 
@@ -281,6 +131,108 @@ function resetBoard() {
 
 //---------------------------------------------------------------------------------------------------------
 
+/**Removes the Start button when it's clicked on and counts down "3, 2, 1, GO!" with one second between each of them. */
+function startGame() {
+    // remove the start button after one second and display "3" in its place.
+    startButton.remove();
+    let countdown = document.createElement('p');
+    countdown.classList.add('start-countdown');
+    countdown.innerHTML = "3";
+    let cardsGrid = document.getElementsByClassName('cards-grid');
+    cardsGrid[0].append(countdown);
+    setTimeout(() => {
+        // remove the "3" after one second and display "2" in its place.
+        countdown.innerHTML = "2";
+        setTimeout(() => {
+            // remove the "2" after one second and display "1" in its place.
+            countdown.innerHTML = "1";
+            setTimeout(() => {
+                // remove the "1" after one second and display "GO!" in its place.
+                countdown.innerHTML = "GO!";
+                setTimeout(() => {
+                    // remove the countdown.
+                    countdown.remove();
+                }, 1000);
+                // immediately after "GO!" is removed, start the timer by triggering the startTimer 
+                // function and let the user play by triggering the flipCard function.
+                startTimer();
+                boardLocked = false;
+                flipCard();
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}
+
+/**Starts the countdown from 60 seconds. If 0 is reached, the gameOver function is triggered. */
+function startTimer() {
+    let restartButton = document.createElement('button');
+    restartButton.setAttribute('class', 'restart');
+    restartButton.innerHTML = "RESTART";
+    let headerButtons = document.getElementsByClassName('header-buttons');
+    headerButtons[0].append(restartButton);
+    restartButton.addEventListener('click', restartMidGame);
+
+    console.log('start timer now!');
+    let gameTimer = document.getElementsByClassName('timer');
+    let maxTime = 30;
+    let timer = setInterval(() => {
+        /* If the timer has more than 0 seconds remaining, show how many seconds are left
+         in the gameTimer element. Then, deduct 1 second from the timer.*/
+        if (maxTime > 0) {
+            // If there is less than 10 seconds on the timer, add a '0' before the number of seconds.
+            gameTimer[0].innerHTML = `00:${maxTime < 10 ? '0' + maxTime : maxTime}`;
+            maxTime--;
+            // If all 24 cards have the card-flipped class, trigger the youWin function.
+            let cardFlipped = document.getElementsByClassName('card-flipped');
+            if (maxTime > 0 && cardFlipped.length === 24) {
+                clearInterval(timer);
+                gameTimer[0].innerHTML = "00:00";
+                youWin();
+                restartButton.remove();
+            }
+            if (maxTime > 0 && startAgain === true) {
+                clearInterval(timer);
+                gameTimer[0].innerHTML = "00:00";
+                startAgain = false;
+            }
+            /* If the timer reaches 0, show "00:00!" in the timer area, stop the timer
+            and trigger the gameOver function.*/
+        } else {
+            gameTimer[0].innerHTML = "00:00";
+            clearInterval(timer);
+            gameOver();
+            restartButton.remove();
+        }
+    }, 1000);
+}
+
+let startAgain = false;
+
+/**Restarts the game and the timer when the player is in the middle of the current game. */
+function restartMidGame() {
+    console.log('restart mid-game now');
+    boardLocked = true;
+    // Remove the retry button as it has been clicked.
+    let restartButton = document.getElementsByClassName('restart');
+    restartButton[0].remove();
+    startAgain = true;
+    let cards = document.getElementsByClassName('card');
+    for (let i = 0; i < cards.length; i++) {
+        // cards[i].removeEventListener('click', flipCard);
+        // get all the cards and remove the 'flip' class, so they are all facing the same way.
+        cards[i].classList.remove('flip');
+        setTimeout(() => {
+            // make all cards visible again after 0.5 sec delay, so player cannot see the image on the cards as they turn.  
+            cards[i].style.visibility = "visible";
+        }, 500);
+    }
+    // re-add the startbutton and its event listener, so the startGame function can be run again.
+    // set timeout so button appears after cards have been returned to original positions.
+    setTimeout(() => {
+        cardsGrid[0].prepend(startButton);
+    }, 500);
+}
+
 /** Shows the player a 'you won!' message and lets them restart the game by clicking the 'play again' button. */
 function youWin() {
     console.log('gameOver triggered');
@@ -307,4 +259,54 @@ function youWin() {
         cardsGrid[0].append(retryButton);
         retryButton.addEventListener('click', restartGame);
     }, 2000);
+}
+
+/**Stops the player from flipping cards and tells them the game is over. Gives the player the option to play again with a retry button. */
+function gameOver() {
+    console.log('gameOver triggered');
+    // get all elements with the class 'card' and iterate through them, adding the event listener for all.
+    let cards = document.getElementsByClassName('card');
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].removeEventListener('click', flipCard);
+    }
+    // show "YOU LOST!" to the player.
+    let losingMessage = document.createElement('p');
+    losingMessage.classList.add('losing-message');
+    losingMessage.innerHTML = "YOU LOST!";
+    let cardsGrid = document.getElementsByClassName('cards-grid');
+    cardsGrid[0].append(losingMessage);
+    setTimeout(() => {
+        // remove the losingMessage after one second and display retryButton in its place.
+        losingMessage.remove();
+        console.log('losingMessage removed');
+        let retryButton = document.createElement('button');
+        retryButton.setAttribute('id', 'retry-button');
+        retryButton.innerHTML = "TRY AGAIN?";
+        cardsGrid[0].append(retryButton);
+        retryButton.addEventListener('click', restartGame);
+    }, 2000);
+}
+
+/**Removes the 'try again' button once clicked, reset the game area and re-add the start button with its event listener so the game can be run again. */
+function restartGame() {
+    console.log('restart game now');
+    // Remove the retry button as it has been clicked.
+    let retryButton = document.getElementById('retry-button');
+    retryButton.remove();
+    let cards = document.getElementsByClassName('card');
+    for (let i = 0; i < cards.length; i++) {
+        // cards[i].removeEventListener('click', flipCard);
+        // get all the cards and remove the 'flip' class, so they are all facing the same way.
+        cards[i].classList.remove('flip');
+        setTimeout(() => {
+            // make all cards visible again after 0.5 sec delay, so player cannot see the image on the cards as they turn.  
+            cards[i].style.visibility = "visible";
+        }, 500);
+    }
+    // re-add the startbutton and its event listener, so the startGame function can be run again.
+    // set timeout so button appears after cards have been returned to original positions.
+    setTimeout(() => {
+        cardsGrid[0].prepend(startButton);
+        startGame();
+    }, 500);
 }
